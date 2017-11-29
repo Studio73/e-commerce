4cccaaf4779473420243d4823f47e3522f991516
Pablo Fuentes <pablo@studio73.es>
2017-11-29 20:31:21 +0100

    [IMP] Simplificar

--- entrypoint.py
+++ entrypoint.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 import os.path
-import subprocess
+import subprocess as sp
 import sys
 
 
 if __name__ == '__main__':
     # Hack to fix wrong uid/gid inside container
-    uid = subprocess.Popen(['id', '-u'], stdout=subprocess.PIPE).stdout.read()
+    uid = sp.Popen(['id', '-u'], stdout=sp.PIPE).stdout.read()
     host_uid = os.environ.get('HOST_UID', False)
-    gid = subprocess.Popen(['id', '-g'], stdout=subprocess.PIPE).stdout.read()
+    gid = sp.Popen(['id', '-g'], stdout=sp.PIPE).stdout.read()
     host_gid = os.environ.get('HOST_GID', False)
     if host_uid and host_uid != uid:
-        subprocess.call(
+        sp.call(
             ['usermod', '-u', '%s' % host_uid, 'odoo']
         )
     if host_gid and host_gid != gid:
-        subprocess.call(
+        sp.call(
             ['groupmod', '-g', '%s' % host_gid, 'odoo']
         )
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             args.append('--update')
         else:
             args.append('--init')
-        subprocess.call(args)
+        sp.call(args)
 
     args = ['gosu', 'odoo:odoo'] + sys.argv[1:]
-    subprocess.call(args)
+    sp.call(args)
