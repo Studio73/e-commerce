a5bfc4cc4809c0755f326ffa2e15e21ba7a5ac22
Pablo Fuentes <pablo@studio73.es>
2017-11-29 21:33:00 +0100

    [IMP] Condicion innecesaria....

--- entrypoint.py
+++ entrypoint.py
@@ -19,13 +19,12 @@ if __name__ == '__main__':
             ['groupmod', '-g', '%s' % host_gid, 'odoo']
         )
 
-    if not os.environ.get('NO_UPDATE', False):
-        args = ['gosu', 'odoo:odoo', 'oman']
-        if os.environ.get('FORDE_UPDATE', False):
-            args.append('--update')
-        else:
-            args.append('--init')
-        sp.call(args)
+    args = ['gosu', 'odoo:odoo', 'oman']
+    if os.environ.get('UPDATE', False):
+        args += ['--update', 'all']
+    else:
+        args.append('--init')
+    sp.call(args)
 
     args = ['gosu', 'odoo:odoo'] + sys.argv[1:]
     sp.call(args)
