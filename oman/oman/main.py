036c329d4b7ab6fa68a79053d5fd2bc34c10df76
Pablo Fuentes <pablo@studio73.es>
2017-11-29 21:31:47 +0100

    [IMP] Limpieza

--- oman/oman/main.py
+++ oman/oman/main.py
@@ -7,39 +7,42 @@ import psycopg2
 import subprocess
 
 from os.path import expandvars
+SRC_PATH = os.environ['SRC_PATH']
+DATA_PATH = os.environ['DATA_PATH']
+SETUP_PATH = os.environ['SETUP_PATH']
 
 
-def clone_odoo(force_update=False):
-    odoo_path = expandvars('$SRC_PATH/odoo')
-    if force_update or not os.path.exists(
+def clone_odoo(force=False):
+    odoo_path = os.path.join(SRC_PATH, 'odoo')
+    if force or not os.path.exists(
         os.path.join(odoo_path, 'odoo-bin')
     ):
-        odoo_yaml = expandvars('$SETUP_PATH/odoo.yaml')
+        odoo_yaml = os.path.join(SETUP_PATH, 'odoo.yaml')
         if not os.path.exists(odoo_yaml):
-            print '[!] No odoo.yaml provided'
+            print('[!] No odoo.yaml provided')
         else:
             output = subprocess.check_call(
                 ['gitaggregate', '-c', odoo_yaml, '--expand-env']
             )
             if output == 0:
-                print "[+] Odoo succesfully cloned"
+                print("[+] Odoo succesfully cloned")
             else:
                 raise Exception
     else:
-        print "[+] Skipped, Odoo already cloned..."
+        print("[+] Skipping, Odoo already cloned...")
 
 
-def clone_addons(force_update=False):
-    odoo_path = expandvars('$SRC_PATH/odoo')
-    repos_setup = glob.glob(
-        os.path.join(expandvars('$SETUP_PATH'), 'repos*.yaml')
+def clone_addons(force=False):
+    odoo_path = os.path.join(SRC_PATH, 'odoo')
+    repos_list = glob.glob(
+        os.path.join(SETUP_PATH, 'repos*.yaml')
     )
-    repos_setup.sort()
+    repos_list.sort()
     conf = kaptan.Kaptan(handler="yaml")
     addons = []
 
-    for repo_yaml in repos_setup:
-        if force_update:
+    for repo_yaml in repos_list:
+        if force:
             output = subprocess.check_call(
                 ['gitaggregate', '-c', repo_yaml, '--expand-env']
             )
@@ -57,13 +60,12 @@ def clone_addons(force_update=False):
                     if output != 0:
                         raise Exception
                 else:
-                    print "[+] Skipped, %s already cloned..." % repo_exp
+                    print("[+] Skipping, %s already cloned..." % repo_exp)
     addons.append(os.path.join(odoo_path, 'addons'))
     return addons
 
 
 def init_git_conf():
-
     cfg = open(os.path.expanduser('~/.gitconfig'), 'w+')
     cfg.writelines([
         '[user]\n',
@@ -73,15 +75,18 @@ def init_git_conf():
 
 
 def copy_ssh_key():
-    ssh_path = os.path.join(expandvars('$SETUP_PATH'), '.ssh/')
+    ssh_path = os.path.join(SETUP_PATH, '.ssh/')
     if os.path.exists(ssh_path):
         subprocess.check_call(['cp', '-r', ssh_path, '/opt/odoo/'])
         subprocess.check_call(['chmod', '600', '/opt/odoo/.ssh/id_rsa'])
         subprocess.check_call(['chmod', '600', '/opt/odoo/.ssh/id_rsa.pub'])
 
 
-def build_conf(addons=[]):
-    conf = open(os.path.join(expandvars('$SRC_PATH'), 'odoo.conf'), 'w+')
+def build_conf(addons=None):
+    if addons is None:
+        addons = []
+
+    conf = open(os.path.join(SRC_PATH, 'odoo.conf'), 'w+')
     db_options = {
         'host': expandvars('$DB_HOST'),
         'port': expandvars('$DB_PORT'),
@@ -91,7 +96,7 @@ def build_conf(addons=[]):
     }
     options = [
         '[options]\n'
-        'data_dir=%s\n' % os.path.join(expandvars('$DATA_PATH'), 'data'),
+        'data_dir=%s\n' % os.path.join(DATA_PATH, 'data'),
         'db_host=%s\n' % db_options['host'],
         'db_port=%s\n' % db_options['port'],
         'db_user=%s\n' % db_options['user'],
@@ -101,9 +106,8 @@ def build_conf(addons=[]):
         'admin_passwd=%s\n' % expandvars('$ADMIN_PSWD'),
         'addons_path=%s\n' % ','.join(addons)
     ]
-
-    # If running for first time and database doesn't
-    # force the language to be used when database creation
+    # If running for first time and database doesn't exists,
+    # force the language to be used on database creation
     try:
         psycopg2.connect(
             "dbname='%s' host='%s' port='%s' user='%s' password='%s'" % (
@@ -112,15 +116,15 @@ def build_conf(addons=[]):
             )
         )
     except psycopg2.OperationalError:
-        if expandvars('$LANG') != '$LANG':
-            options.append('load_language=%s\n' % expandvars('$LANG'))
+        if os.environ.get('LANG', False):
+            options.append('load_language=%s\n' % os.environ['LANG'])
         if not os.environ.get('DEMO', False):
             options.append('without_demo=True\n')
 
     conf.writelines(options)
-    if os.path.exists(os.path.join(expandvars('$SETUP_PATH'), 'odoo.conf')):
+    if os.path.exists(os.path.join(SETUP_PATH, 'odoo.conf')):
         setup_conf = open(
-            os.path.join(expandvars('$SETUP_PATH'), 'odoo.conf')
+            os.path.join(SETUP_PATH, 'odoo.conf')
         )
         conf.writelines([l for l in setup_conf.readlines()])
         setup_conf.close()
@@ -136,34 +140,22 @@ def main():
     )
     parser.add_argument(
         '--update',
-        action='store_true',
-        help='Update Odoo & OCA & Others repositories'
-    )
-    parser.add_argument(
-        '--update-odoo',
-        action='store_true',
-        help='Update Odoo repository'
-    )
-    parser.add_argument(
-        '--update-addons',
-        action='store_true',
-        help='Update OCA & Others repositories'
+        choices=('odoo', 'addons', 'all'),
+        help='Update Odoo or OCA & Others repositories or both'
     )
     args = parser.parse_args()
     if not any([getattr(args, arg) for arg in vars(args)]):
         parser.error('No arguments provided.')
-
-    if args.init or args.update:
-        force = False if args.init else True
+    if args.init:
         init_git_conf()
         copy_ssh_key()
-        clone_odoo(force)
-        addons = clone_addons(force)
+        clone_odoo(False)
+        addons = clone_addons(False)
         build_conf(addons)
-    else:
-        if args.update_odoo:
+    elif args.update:
+        if args.update in ['odoo', 'all']:
             clone_odoo(True)
-        if args.update_addons:
+        if args.update in ['addons', 'all']:
             addons = clone_addons(True)
             build_conf(addons)
 
