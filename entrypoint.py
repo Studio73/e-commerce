15b403a8f3280cd36fd8c9a5eeed07deda5753db
Pablo Fuentes <pablo@studio73.es>
2017-12-12 17:47:06 +0100

    [ADD] Install pip requirements

--- entrypoint.py
+++ entrypoint.py
@@ -2,9 +2,13 @@
 import os.path
 import subprocess as sp
 import sys
+import pip
 
 
-if __name__ == '__main__':
+def main():
+    """
+    Entrypoint main function
+    """
     # Hack to fix wrong uid/gid inside container
     uid = sp.Popen(['id', '-u'], stdout=sp.PIPE).stdout.read()
     host_uid = os.environ.get('HOST_UID', False)
@@ -18,6 +22,7 @@ if __name__ == '__main__':
         sp.call(
             ['groupmod', '-g', '%s' % host_gid, 'odoo']
         )
+    # Cron jobs
     if os.path.exists(os.path.join(os.environ['SETUP'], 'cron')):
         sp.call(
             'printenv > /etc/environment', shell=True
@@ -30,7 +35,18 @@ if __name__ == '__main__':
             '/etc/cron.d/cron'
         ])
         sp.call(['service', 'cron', 'start'])
-
+    # Pip requirements
+    pip_file = os.path.join(os.environ['SETUP'], 'pip.txt')
+    if os.path.exists(pip_file):
+        installed_pip_packages = [
+            "%s==%s" % (i.key, i.version)
+            for i in pip.get_installed_distributions()
+        ]
+        packages = open(pip_file, 'r+').read().splitlines()
+        for package in packages:
+            if package not in installed_pip_packages:
+                pip.main(['install', package])
+    # TODO others requirements apt, npm, etc...
     args = ['gosu', 'odoo:odoo', 'oman']
     if os.environ.get('UPDATE', False):
         args += ['--update', 'all']
@@ -40,3 +56,23 @@ if __name__ == '__main__':
 
     args = ['gosu', 'odoo:odoo'] + sys.argv[1:]
     sp.call(args)
+            for i in pip.get_installed_distributions()
+        ]
+        packages = open(pip_file, 'r+').read().splitlines()
+        for package in packages:
+            if package not in installed_pip_packages:
+                pip.main(['install', package])
+    # TODO others requirements apt, npm, etc...
+    args = ['gosu', 'odoo:odoo', 'oman']
+    if os.environ.get('UPDATE', False):
+        args += ['--update', 'all']
+    else:
+        args.append('--start')
+    sp.call(args)
+
+    args = ['gosu', 'odoo:odoo'] + sys.argv[1:]
+    sp.call(args)
+
+
+if __name__ == '__main__':
+    main()
