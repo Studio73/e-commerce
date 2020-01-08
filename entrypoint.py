fe73a11fdc9d70b1c00cd22f4a958f0483f2b09a
Pablo Fuentes <pablo@studio73.es>
2020-01-08 21:16:38 +0100

    [IMP] entrypoint
    Por simplificar se ha movido al proceso de instalación

--- entrypoint.py
+++ entrypoint.py
@@ -11,22 +11,7 @@ def main():
     Entrypoint main function
     """
     dodoo_tools.install.main()
-    if os.environ.get("DEBUG") and "python" in sys.argv[1]:
-        cmd = [
-            "gosu",
-            "odoo:odoo",
-            sys.argv[1],
-            "-m",
-            "ptvsd",
-            "--host",
-            "0.0.0.0",
-            "--port",
-            "5678",
-        ]
-        cmd += sys.argv[2:]
-    else:
-        cmd = ["gosu", "odoo:odoo"] + sys.argv[1:]
-
+    cmd = ["gosu", "odoo:odoo"] + sys.argv[1:]
     if os.environ.get("ODOO_VERSION") == "8.0":
         cmd = [c.replace("odoo-bin", "odoo.py") for c in cmd]
     return sp.call(cmd)
