0f11d47c2a688e302209b858d913c51ef932d273
Pablo Fuentes <pablo@studio73.es>
2020-03-17 18:14:04 +0100

    [ADD] Opción para modificar uid y gid al arrancar

--- entrypoint.py
+++ entrypoint.py
@@ -10,6 +10,14 @@ def main():
     """
     Entrypoint main function
     """
+    # Hack to fix wrong uid/gid inside container
+    host_uid = os.environ.get("HOST_UID", os.getuid())
+    host_gid = os.environ.get("HOST_GID", os.getgid())
+    if host_uid != os.getuid():
+        dodoo_tools._utils.run(["usermod", "-u", str(host_uid), "odoo"])
+    if host_gid != os.getgid():
+        dodoo_tools._utils.run(["groupmod", "-g", str(host_uid), "odoo"])
+
     dodoo_tools.install.main()
     cmd = ["gosu", "odoo:odoo"] + sys.argv[1:]
     if os.environ.get("ODOO_VERSION") == "8.0":
