8d972950ac3142b81d2a4502d6d38d97681e9262
Pablo Fuentes <pablo@studio73.es>
2020-05-01 17:41:37 +0200

    [IMP] tools database: nuevas opciones
    * --download: Descargar de S3 la copia de seguridad
    * --location: Personalizar la ruta donde se encuentra la base de datos
    * --source: Nombre de la base de datos que se utilizará para restaurar, de esta forma podemos especificar el nombre que queramos de la nueva base de datos sin necesidad de tener una copia previa con su nombre

--- dodoo-tools/dodoo_tools/database.py
+++ dodoo-tools/dodoo_tools/database.py
@@ -4,6 +4,8 @@ import os
 import calendar
 import click
 from datetime import datetime
+from getpass import getpass
+from minio import Minio
 
 from ._utils import echo, run
 from .cli import cli
@@ -53,13 +55,26 @@ def backup(dbname, force, filestore):
     "-F", "--force", is_flag=True, help="Delete database if exists and create a new one"
 )
 @click.option("-f", "--filestore", is_flag=True, help="Restore also the filestore")
+@click.option(
+    "-d", "--download", is_flag=True, help="Download the database from S3 storage"
+)
 @click.option("-w", "--weekday", type=int, help="Weekday, default today (Monday is 0)")
 @click.option("-t", "--template", help="Use a template as a restore source")
+@click.option("-l", "--location", help="Location where are the database backups")
+@click.option("-s", "--source", help="Project database name to use as source")
 @click.argument("dbname", envvar="DATABASE")
-def restore(dbname, force, filestore, weekday, template):
+def restore(dbname, force, filestore, download, weekday, template, location, source):
+    source = source or dbname
+    location = location or os.path.join(os.environ["DATA"], "backup")
     if weekday is None:
         weekday = datetime.today().weekday()
     day = calendar.day_name[int(weekday)].upper()
+    backup_name = "%s_%s.tar.gz" % (source, day)
+    fstore_name = "%s-fstore-%s.tar.gz" % (source, day)
+    if download:
+        download_from_s3(backup_name, location, "backup")
+        if filestore:
+            download_from_s3(fstore_name, location, "filestore")
     with echo("Restoring database backup (%s)" % dbname):
         pguser = os.environ["PGUSER"]
         if _database_exists(dbname):
@@ -78,12 +93,11 @@ def restore(dbname, force, filestore, weekday, template):
                 check_call=True,
             )
         else:
-            backup_name = "%s_%s.tar.gz" % (dbname, day)
-            backup_path = os.path.join(os.environ["DATA"], "backup", backup_name)
+            backup_path = os.path.join(location, backup_name)
             if not os.path.isfile(backup_path):
                 raise Exception(
                     "Selected backup %s for %s (%s) does not exist, please check"
-                    % (dbname, day.capitalize(), weekday)
+                    % (source, day.capitalize(), weekday)
                 )
             run(["createdb", "-U", pguser, "-O", pguser, dbname], check_call=True)
             run(
@@ -92,25 +106,43 @@ def restore(dbname, force, filestore, weekday, template):
             )
     if filestore:
         with echo("Restoring filestore backup (%s)" % dbname):
-            data_path = os.environ["DATA"]
-            backup_path = os.path.join(data_path, "backup")
-            fstore_name = os.path.join(
-                backup_path, "%s-fstore-%s.tar.gz" % (dbname, day)
-            )
-            if os.path.isfile(fstore_name):
-                fstore_dest = os.path.join(data_path, "data", "filestore")
+            fstore_path = os.path.join(location, fstore_name)
+            if os.path.isfile(fstore_path):
+                fstore_dest = os.path.join(os.environ["DATA"], "data", "filestore")
                 run(["mkdir", "-p", fstore_dest])
-                run(["tar", "-xf", fstore_name, "-C", "/tmp"])
-                source_path = os.path.join("/tmp", dbname)
+                run(["tar", "-xf", fstore_path, "-C", "/tmp"])
+                source_path = os.path.join("/tmp", source)
                 if not os.path.exists(source_path):
                     # Old backups data structure
-                    source_path = os.path.join("/tmp", "data", "filestore", dbname)
+                    source_path = os.path.join("/tmp", "data", "filestore", source)
                     if not os.path.exists(source_path):
                         raise Exception("Unknown filestore data structure")
                 run(["mv", source_path, fstore_dest])
                 host_uid = os.environ.get("HOST_UID", "odoo")
                 host_gid = os.environ.get("HOST_GID", "odoo")
                 run(["chown", "-R", "%s:%s" % (host_uid, host_gid), fstore_dest])
+
+
+def download_from_s3(name, dest, ftype):
+    url = os.environ.get("S3_URL") or input("? S3 url: ")
+    user = os.environ.get("S3_USER") or input("? S3 user: ")
+    secret = os.environ.get("S3_SECRET") or getpass("? S3 secret: ")
+    with echo("Downloading database %s %s" % (ftype, name)):
+        client = Minio(url, access_key=user, secret_key=secret, secure=True)
+        download_obj = False
+        for bucket in client.list_buckets():
+            for fil in client.list_objects(bucket.name, recursive=True):
+                if name in fil.object_name:
+                    download_obj = fil
+                    break
+            if download_obj:
+                break
+        if not download_obj:
+            raise Exception("%s file not found" % name)
+        data = client.get_object(download_obj.bucket_name, download_obj.object_name)
+        with open(os.path.join(dest, name), "wb") as file_data:
+            for d in data.stream(32 * 1024):
+                file_data.write(d)
 
 
 if __name__ == "__main__":
--- dodoo-tools/setup.py
+++ dodoo-tools/setup.py
@@ -10,6 +10,6 @@ setup(
     url="https://www.studio73.es",
     include_package_data=True,
     packages=find_packages(),
-    install_requires=["click>=7"],
+    install_requires=["click>=7", "minio"],
     entry_points={"console_scripts": ["dodoo-tools = dodoo_tools.cli:cli"]},
 )
