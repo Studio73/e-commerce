bf2d659154eda341780c683750ac8d1710b12cfb
Pablo Fuentes <pablo@studio73.es>
2020-06-21 12:54:27 +0200

    [REF] Simplificar estructura de carpetas

--- build.sh
+++ build.sh
@@ -3,11 +3,16 @@ DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 REF=`git rev-parse --short HEAD`
 VERSION=$1
 TAG=$1
+DEST="py3"
 if [ "$1" == "runbot" ]; then
     VERSION="13.0"
+    DEST="runbot"
+fi
+if [ "$VERSION" = "8.0" ] || [ "$VERSION" = "10.0" ]; then
+    DEST="py2"
 fi
 
 docker build --build-arg BUILD_DATE=$DATE \
              --build-arg VCS_REF=$REF \
              --build-arg VERSION=$VERSION \
-             -f $TAG/Dockerfile . -t dodoo:$TAG
+             -f $DEST/Dockerfile . -t dodoo:$TAG
