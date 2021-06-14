d9b3923ed8e427f4a5b502f378345a14d0beaed2
Pablo Fuentes <pablo@studio73.es>
2021-06-14 12:14:42 +0200

    [REM] entrypoint: useless condition

--- entrypoint.py
+++ entrypoint.py
@@ -23,8 +23,6 @@ def main():
         return sp.call(sys.argv[1:])
     dodoo_tools.install.main()
     cmd = ["gosu", "odoo:odoo"] + sys.argv[1:]
-    if os.environ.get("ODOO_VERSION") == "8.0":
-        cmd = [c.replace("odoo-bin", "odoo.py") for c in cmd]
     return sp.call(cmd)
 
 
