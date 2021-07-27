6cbb767d67d82192c1abd8e6ed97087384cceab6
Pablo Fuentes <pablo@studio73.es>
2021-07-27 15:54:02 +0200

    [FIX] build: Odoo 13.0 with Python 3.6
    Fix some package dependencies problems due Python 3.8

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ TAG=$1
 if [ "$VERSION" = "8.0" ] || [ "$VERSION" = "9.0" ] || [ "$VERSION" = "10.0" ]; then
     DEST="py2"
 fi
-if [ "$VERSION" = "11.0" ] || [ "$VERSION" = "12.0" ]; then
+if [ "$VERSION" = "11.0" ] || [ "$VERSION" = "12.0" ] || [ "$VERSION" = "13.0" ]; then
     DEST="py3.6"
 fi
-if [ "$VERSION" = "13.0" ] || [ "$VERSION" = "14.0" ]; then
+if [ "$VERSION" = "14.0" ]; then
     DEST="py3.8"
 fi
 
