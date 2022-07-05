d9b309a1ae2c459d001df0f5677152e95700073b
Pablo Fuentes <pablo@studio73.es>
2022-07-05 15:32:55 +0200

    [ADD] proxy: Image to passthrougth connections inside internal networks

---
+++ proxypass/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# (c) Studio73 - Pablo Fuentes <pablo@studio73.es>
+set -e
+if [ -z $TARGET ]; then
+    echo "Missing TARGET enviroment variable"
+    exit 75
+else
+    sed -i 's/TARGET/'"$TARGET"'/g' /etc/nginx/conf.d/default.conf
+fi
+
+if [ $PORT ]; then
+
+    echo -e "
+server {
+    listen $PORT;
+    proxy_pass $TARGET:$PORT;
+}
+" >> /etc/nginx/conf.d/port.conf
+
+fi
+
+# Override dns nameserver to avoid Docker to resolve itself
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+exec "$@"
