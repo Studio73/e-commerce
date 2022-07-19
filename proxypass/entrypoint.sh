5cec49492b7805a440c5869a15e41aca14eea23c
Pablo Fuentes <pablo@studio73.es>
2022-07-19 17:16:31 +0200

    [FIX] proxypass: Avoid duplicated address and port pair

--- proxypass/entrypoint.sh
+++ proxypass/entrypoint.sh
@@ -8,15 +8,13 @@ else
     sed -i 's/TARGET/'"$TARGET"'/g' /etc/nginx/conf.d/default.conf
 fi
 
-if [ $PORT ]; then
-
+if [ $PORT ] && [ ! -f /etc/nginx/conf.d/port.conf ]; then
     echo -e "
 server {
     listen $PORT;
     proxy_pass $TARGET:$PORT;
 }
 " >> /etc/nginx/conf.d/port.conf
-
 fi
 
 # Override dns nameserver to avoid Docker to resolve itself
