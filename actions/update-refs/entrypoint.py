38f520d6e40a27c13d64a2d33549a09d9cbbcbbc
Pablo Fuentes <pablo@studio73.es>
2023-11-20 16:06:08 +0100

    feat(actions): Update refs add 17.0

--- actions/update-refs/entrypoint.py
+++ actions/update-refs/entrypoint.py
@@ -11,7 +11,17 @@ import requests
 import yaml
 from plumbum import local
 
-AVAILABLE_VERSIONS = ["9.0", "10.0", "11.0", "12.0", "13.0", "14.0", "15.0", "16.0"]
+AVAILABLE_VERSIONS = [
+    "9.0",
+    "10.0",
+    "11.0",
+    "12.0",
+    "13.0",
+    "14.0",
+    "15.0",
+    "16.0",
+    "17.0",
+]
 AVAILABLE_ORGS = ["odoo", "oca", "openupgrade"]
 git = local["git"]
 
